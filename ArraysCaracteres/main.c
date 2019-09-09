a368c90cbd0429bcc1baa38d2846e5a2564b8ec6
Franco Canevali <mail@francocanevali.com>
2019-09-09 09:05:05 -0300

    ejercitacion

---
+++ ArraysCaracteres/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+
+/**
+    system("clear");
+    system("read -p 'Press Enter to continue...' var");
+*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
